dcac627a65efea92bc10de51cfaa52eba7a54d64
zsy <178456094@qq.com>
2023-05-23 16:11:01 +0800

    初版发布

--- lib/toast_utils.dart
+++ lib/toast_utils.dart
@@ -1,3 +1,30 @@
 library toast_utils;
+import 'package:fluttertoast/fluttertoast.dart';
 
+typedef ShowToastFunction = Function(
+  String msg, {
+  Toast toastLength,
+  ToastGravity gravity,
+});
 
+class ToastUtil {
+  static ShowToastFunction? showToastFunction;
+
+  static void show(
+    String msg, {
+    Toast toastLength = Toast.LENGTH_SHORT,
+    ToastGravity gravity = ToastGravity.BOTTOM,
+  }) {
+    if (showToastFunction != null) {
+      showToastFunction?.call(msg, toastLength: toastLength, gravity: gravity);
+      return;
+    }
+
+    Fluttertoast.cancel();
+    Fluttertoast.showToast(
+      msg: msg,
+      toastLength: toastLength,
+      gravity: gravity,
+    );
+  }
+}
